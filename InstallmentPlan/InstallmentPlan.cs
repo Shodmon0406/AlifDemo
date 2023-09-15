144173ca6061b263857b20dc902da925167b3dae
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2023-09-15 18:52:50 +0500

    Добавьте файлы проекта.

---
+++ InstallmentPlan/InstallmentPlan.cs
@@ -0,0 +1,43 @@
+﻿public class InstallmentPlan
+{
+    private string Product { get; set; }
+    private decimal Money { get; set; }
+    private int Month { get; set; }
+    private string PhoneNumber { get; set; }
+    private int Percent { get; set; }
+    private int Interval { get; set; }
+
+    public InstallmentPlan(int product, decimal money, int month, string phoneNumber)
+    {
+        Money = money;
+        Month = month;
+        PhoneNumber = phoneNumber;
+        if (product == 1)
+        {
+            Product = "Phone";
+            Percent = 3;
+            Interval = 9;
+        }
+        else if (product == 2)
+        {
+            Product = "Laptop";
+            Percent = 4;
+            Interval = 12;
+        }
+        else if (product == 3)
+        {
+            Product = "Television";
+            Percent = 5;
+            Interval = 18;
+        }
+    }
+
+    public string Calculate()
+    {
+        var dec = Math.Ceiling(Month / (decimal)Interval);
+        var total = Money + (Money * (Math.Ceiling(Month / (decimal)Interval) * Percent) / 100);
+        var perMonth = Math.Round(total / (decimal)Month, 2);
+        return $"You bought a {Product} and have to pay {total} somoni within {Month} months. every month {perMonth} somoni.";
+    }
+}
+
---
+++ InstallmentPlan/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static int Main()
+    {
+        Console.Write("Please enter product id (phone = 1, laptop = 2, television = 3) : ");
+        var product = int.Parse(Console.ReadLine());
+        Console.Write("Please enter the product price : ");
+        var money = decimal.Parse(Console.ReadLine());
+        Console.Write("Please enter the month : ");
+        var month = int.Parse(Console.ReadLine());
+        Console.Write("Please enter ypur phone number : ");
+        var phoneNumber = Console.ReadLine();
+        var a = product < 1 && product > 3;
+        var c = month < 3 && month > 24;
+        var d = money < 0;
+        var e = string.IsNullOrEmpty(phoneNumber);
+        if (product < 1 || product > 3 || month < 3 || month > 24 || money < 0 || string.IsNullOrEmpty(phoneNumber))
+        {
+            Console.WriteLine("Invalid input!");
+            return Main();
+        }
+
+        var totalPrice = new InstallmentPlan(product, money, month, phoneNumber);
+        Console.WriteLine(totalPrice.Calculate());
+        return Main();
+    }
+}
